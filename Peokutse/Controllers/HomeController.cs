174b385c48b017f2cd3a293c9707d5ecbe8ab1f5
ripmyloven <ripmyloven@gmail.com>
2025-01-08 11:32:27 +0200

    commit

--- Peokutse/Controllers/HomeController.cs
+++ Peokutse/Controllers/HomeController.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Peokutse.Models;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace Peokutse.Controllers
@@ -10,21 +13,43 @@ namespace Peokutse.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.Message = "Ootan sind minu poele! Palun tule!!!";
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 10 ? "Tere hommikust!" : "Tere päevast!";
             return View();
         }
-
-        public ActionResult About()
+        [HttpGet]
+        public ViewResult Ankeet()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            E_Mail(guest);
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guest);
+            }
+            else
+            {
+                return View();
+            }
         }
-
-        public ActionResult Contact()
+        public void E_Mail(Guest guest)
         {
-            ViewBag.Message = "Your contact page.";
+            try
+            {
+                WebMail.SmtpServer = "smtp.gmail.com";
+                WebMail.SmtpPort = 587;
+                WebMail.EnableSsl = true;
+                WebMail.UserName = "ripmyloven@gmail.com";
+                WebMail.Password = "*****";
+                WebMail.From = "ripmyloven@gmail.com";
+                WebMail.Send("ripmyloven@gmail.com", "Vastus kutsele", guest.Name + " Vastus" + ((guest.WillAttend ?? false) ?
+                    "tuleb poele " : "ei tule poele"));
+                ViewBag.Message = "Kiri on saatnud!";
 
-            return View();
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = "Mul on kuhju! Ei saa kirja saada!!!";
+            }
         }
     }
 }
